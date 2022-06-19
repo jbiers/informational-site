df01be189efa2f428f976af37e05da0c6c996af9
jbiers <juliabiersuriano@gmail.com>
2022-06-19 00:52:36 -0400

    set up server

---
+++ app.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import * as http from 'http';
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`${new Date()}\n`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
