7e1d9c9aea3080e29ca83982ce705b2b7ece115b
jbiers <juliabiersuriano@gmail.com>
2022-06-30 17:25:07 -0400

    fix file structure

--- test/urls.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { URL } from 'node:url';
-
-const username = 'jbiers';
-
-const myProfileURL = new URL(`/${username}`, 'https://github.com');
-console.log(myProfileURL);
